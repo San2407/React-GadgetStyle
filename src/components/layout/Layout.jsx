803ca68ba012242963708afcf144e10cf148f8f0
SanAlba24 <salbarracin24@gmail.com>
2024-03-20 20:02:07 -0300

    se agregaron mas productos

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import { AgregarDocs } from "../../agregarDocs";
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <AgregarDocs />
       <Outlet />
-      {/*agregar el footer*/}
     </>
   );
 };
--- src/productsMock.js
+++ src/productsMock.js
@@ -1,4 +1,5 @@
 export let products = [
+    //celulares
     {
         category: "Celulares",
         title: 'TCL 40 SE 256 GB dark gray 6 GB RAM',
@@ -6,6 +7,38 @@ export let products = [
         price: 250.999,
         img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1708901361/GadgetStyle/celulares/faln6cxtaosnjvnzeyn7.jpg",
         stock: 10
+    },
+    {
+        category: "Celulares",
+        title: 'Celular Xiaomi Redmi 12C 128GB Graphite Gray',
+        description: 'Dispositivo liberado para que elijas la compañía telefónica que prefieras.Pantalla IPS de 6.71".Tiene 2 cámaras traseras de 50Mpx/2Mpx.Cámara delantera de 5Mpx.Procesador Mediatek MT6769Z Helio G85 Octa-Core de 2GHz con 4GB de RAM.Batería de 5000mAh.Memoria interna de 128GB.Con sensor de huella dactilar.',
+        price: 269.999,
+        img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1710941441/GadgetStyle/celulares/a3ra5prhfkqoappleafo.webp",
+        stock: 8
+    },
+    {
+        category: "Celulares",
+        title: 'Celular Samsung Galaxy S21 Fe 128gb - Batería 4500 Mah Color Blanc',
+        description: 'Memoria RAM: 6 GB. Dispositivo liberado para que elijas la compañía telefónica que prefieras. Compatible con redes 5G.Pantalla AMOLED de 6.4".Tiene 3 cámaras traseras de 12Mpx/12Mpx/8Mpx. Cámaras delanteras de 32Mpx. Procesador Exynos 2100 Octa-Core de 2.9GHz con 6GB de RAM. Batería de 4500mAh con carga inalámbrica. Memoria interna de 128GB. A prueba de agua con reconocimiento facial y sensor de huella dactilar. Resistente al polvo.',
+        price: 963.049,
+        img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1710942095/GadgetStyle/celulares/dvsh1rm3hhkos27ydfm2.webp",
+        stock: 4
+    },
+    {
+        category: "Celulares",
+        title: 'Moto G14 128 GB Rosa 4 GB RAM',
+        description: 'Memoria RAM: 4 GB. Dispositivo liberado para que elijas la compañía telefónica que prefieras. Pantalla IPS de 6.5". Tiene 2 cámaras traseras de 50Mpx/2Mpx y una cámara delantera de 8MP. Procesador Unisonic T616 Octa-Core de 2GHz con 4GB de RAM. Batería de 5000mAh. Memoria interna de 128GB. Resistente a las salpicaduras con sensor de huella dactilar y resistente al polvo.',
+        price: 299.999,
+        img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1710942366/GadgetStyle/celulares/g6lkqowszpi2nj3ecqck.webp",
+        stock: 3
+    },
+    {
+        category: "Celulares",
+        title: 'Celular Samsung Galaxy A14 128GB Negro',
+        description: 'Dispositivo liberado para que elijas la compañía telefónica que prefieras. Compatible con redes 5G. Pantalla PLS de 6.6". Tiene 3 cámaras traseras de 50Mpx/2Mpx/2Mpx y una cámara delantera de 13Mpx. Batería de 5000mAh. Memoria interna de 128GB. Con reconocimiento facial y sensor de huella dactilar.',
+        price: 359.999,
+        img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1710942435/GadgetStyle/celulares/hpcldh0hovfg7hef1ysi.webp",
+        stock: 7
     },
 
     //fundas
@@ -15,6 +48,38 @@ export let products = [
         description: 'Posee protector de cámara.Con anillo trasero para una mejor sujeción.Su diseño elegante y funcional que garantiza una protección óptima ',
         price: 7.350,
         img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1708877283/GadgetStyle/fundas/vxca7hyjxlbsumyvvwnk.webp",
+        stock: 8
+    },
+    {
+        category: "Fundas",
+        title: 'Funda Apple iPhone 11 Silicona',
+        description: 'Diseñado por Apple para complementar el iPhone 11, la forma de la funda de silicona se ajusta perfectamente a los botones de volumen, el botón lateral y las curvas de su dispositivo sin agregar volumen. Un forro de microfibra suave en el interior ayuda a proteger tu iPhone. En el exterior, el acabado sedoso y suave al tacto del exterior de silicona se siente genial en tu mano. Por lo tanto, no solo se ve muy bien, está diseñado para proteger su iPhone de arañazos y caídas.',
+        price: 116.900,
+        img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1710943407/GadgetStyle/fundas/rsqdy7ymlzh6dbxnrwhc.webp",
+        stock: 5
+    },
+    {
+        category: "Fundas",
+        title: 'Funda Samsung Smart Clear View Cover Para Galaxy S20 Fe',
+        description: 'Samsung es reconocida a nivel mundial como una empresa líder en la industria tecnológica. Tanto sus dispositivos como los accesorios que desarrollan para los mismos, son diseñados con una calidad superior y pensados para contribuir a una experiencia única y satisfactoria.',
+        price: 20.019,
+        img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1710943640/GadgetStyle/fundas/qqprc91d9zb50osroch1.webp",
+        stock: 9,
+    },
+    {
+        category: "Fundas",
+        title: 'Funda Silicona Para Motorola Silicone Case Antishock',
+        description: 'Diseño súper elegante Suave al tacto. Interior revestido en microfibra asegura máxima protección. Elevado para proteger la lente de la cámara y todos los puertos quedan perfectamente expuestos para un uso sin obstáculos',
+        price: 3.400,
+        img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1710945124/GadgetStyle/fundas/hz97t4oyrbl0kx2yee7p.webp",
+        stock: 22
+    },
+    {
+        category: "Fundas",
+        title: 'Funda Tpu Transp Antig Con Marco Protec Lente Para Redmi 12c',
+        description: 'Protege tu valioso Xiaomi Redmi 12C con esta funda transparente de alta calidad. Diseñada específicamente para este modelo, ofrece una combinación perfecta de estilo y seguridad. Su diseño minimalista y delgado no compromete la estética del teléfono. Con elevacion de bordes que protegen la pantalla y la camara.',
+        price: 6.499,
+        img: "https://res.cloudinary.com/dsnqnbiib/image/upload/v1710946683/GadgetStyle/fundas/lszaihobb0twdufvs8rm.webp",
         stock: 8
     },
 ]
